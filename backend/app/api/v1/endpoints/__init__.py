6436d89dab53157f85816a5f1a6949caf0d7bd02
alex6712 <vanyukov.alex@gmail.com>
2023-07-27 19:05:18 +0300

    🧰 Project restructuring

--- backend/app/api/v1/endpoints/__init__.py
+++ backend/app/api/v1/endpoints/__init__.py
@@ -1,6 +1,6 @@
-"""Genshin Impact Characters Analyzer API routers
+"""Genshin Impact Characters Analyzer API V1 routers
 
-A package with the implementation of API routers.
+A package with the implementation of API V1 routers.
 
 This package collects implementations of API routers containing a set of
 methods (``endpoints``) of the application's server side.
@@ -13,17 +13,7 @@ because for convenience [1]_ each router is called ``router`` in separate module
 .. [1] For code consistency and readability.
 """
 
-from fastapi import APIRouter
-
 from .auth import router as auth_router
 from .characters import router as characters_router
 from .root import router as root_router
 from .users import router as users_router
-
-api_v1_router = APIRouter(
-    prefix="/api/v1",
-)
-api_v1_router.include_router(auth_router)
-api_v1_router.include_router(characters_router)
-api_v1_router.include_router(root_router)
-api_v1_router.include_router(users_router)
